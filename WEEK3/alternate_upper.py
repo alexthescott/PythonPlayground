8c1678ae98eeff8cccc2aaf0665075d150863255
alexthescott <43523995+alexthescott@users.noreply.github.com>
2020-01-22 20:40:01 -0800

    added alternate_upper.py

---
+++ WEEK3/alternate_upper.py
@@ -0,0 +1,19 @@
+""" alternate_upper.py Alex Scott 2020
+ 
+ write a function which returns a str that alternates the 
+ capitalization of each character, on the even index values
+
+ 'apple' -> 'ApPlE'
+
+"""
+
+def alternate_upper(word):
+	newString = ""
+	for i in range(0, len(word)):
+		if i % 2 == 0: 
+			newString += word[i].upper()
+		else: 
+			newString += word[i]
+	return newString
+
+assert alternate_upper("apple sauce") == "ApPlE SaUcE"
